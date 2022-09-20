37da06317017074eeef8dab8d6e19fc73f240763
Jose Garcia <mendoza.joser51@gmail.com>
2022-09-19 19:44:51 -0700

    game is up and running

--- src/app.py
+++ src/app.py
@@ -1,16 +1,14 @@
+import random as rd
 import time
 import turtle as t
 from body import body
-from moves import go_up, go_down, go_left, go_right
 
-
-# --------------------------- Set screen and snake body ---------------------------
 # --- screen ---
 sc = t.Screen()
 sc.title('Snake Game')
 sc.bgcolor('blue')
 sc.setup(width=600, height=600)
-# sc.tracer(0)
+sc.tracer(0)
 
 # --- head ---
 head = body(shape='square', color='black')
@@ -25,6 +23,26 @@ food.speed(0)
 
 
 # --- Set controls for moving snake ---
+def up():
+    if head.direction != 'down':
+        head.direction = 'up'
+
+
+def down():
+    if head.direction != 'up':
+        head.direction = 'down'
+
+
+def left():
+    if head.direction != 'right':
+        head.direction = 'left'
+
+
+def right():
+    if head.direction != 'left':
+        head.direction = 'right'
+
+
 def move(direction):
     match direction:
         case 'up':
@@ -38,11 +56,43 @@ def move(direction):
 
 
 sc.listen()
-sc.onkeypress(go_up, 'w')
-sc.onkeypress(go_down, 's')
-sc.onkeypress(go_left, 'a')
-sc.onkeypress(go_right, 'd')
+sc.onkeypress(up, 'w')
+sc.onkeypress(down, 's')
+sc.onkeypress(left, 'a')
+sc.onkeypress(right, 'd')
 # ----------------------------------- Main game ---------------------------------
+body_parts = [head]
+continue_game = True
+while continue_game:
+    sc.update()
+    time.sleep(0.2)
+    # head.forward(20)
+
+    # --- check if snake eat food ---
+
+    if head.distance(food) < 20:
+        food_xcor = rd.randint(-270, 270)
+        food_ycor = rd.randint(-270, 270)
+        food.goto(food_xcor, food_ycor)
+
+        new_body = body(shape='square', color='white')
+        new_body.speed(0)
+        new_body.penup()
+        body_parts.append(new_body)
+
+    for index in range(len(body_parts) -1, 0, -1):
+        xcor = body_parts[index -1].xcor()
+        ycor = body_parts[index -1].ycor()
+        body_parts[index].goto(xcor, ycor)
+
+
+
+    move(head.direction)
+
+
+
+    # --- check if snake hit wall ---
+
 
 # expend snake body
 
--- src/moves.py
+++ src/moves.py
@@ -1,19 +1,2 @@
 
-def go_up(head):
-    if head.direction != 'down':
-        head.direction = 'down'
 
-
-def go_down(head):
-    if head.direction != 'up':
-        head.direction = 'down'
-
-
-def go_left(head):
-    if head.direction != 'right':
-        head.direction = 'left'
-
-
-def go_right(head):
-    if head.direction != 'left':
-        head.direction = 'right'
